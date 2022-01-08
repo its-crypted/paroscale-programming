b48a4fe8e6832db5964a47f80722c519a5de73dd
Piyush Thange <pthange19@gmail.com>
2022-01-08 23:24:19 +0530

    linked file
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ assignment/linked.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+	int nubmer;
+	struct node *next;
+}node;
+
+int main(){
+	node *list = NULL;
+
+	node *n = malloc(sizeof(node));
+	if(n == NULL)
+		return 1;
+	n->number = 1;
+	n->next = NULL;
+
+	//update  list to point new node
+	list = n;
+
+	//add a number to a list;
+	n = malloc(sizeof(node));
+	if(n == NULL){
+		free(list);
+		return 1;
+	}
+	n->number = 2;
+	n->next = NULL;
+
+	list->next = n;
+
+	//add a number to list
+	n = malloc(sizeof(node));
+	if(n == NULL){
+		free(list->next);
+		free(list);
+		return 1;
+	}
+	n->number = 3;
+	n->next = NULL;
+	list->next->next = n;
+
+	//print numbers
+	for(node *tmp = list; tmp != NULL; tmp = tmp->next)
+		printf("%i\n", tmp->number); 
+
+}
