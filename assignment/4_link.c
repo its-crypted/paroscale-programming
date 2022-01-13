343ae8007f570cdb85a4cb377dbcda28d61cecd4
Piyush Thange <pthange19@gmail.com>
2022-01-13 15:17:01 +0530

    Linked List completed
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- assignment/4_link.c
+++ assignment/4_link.c
@@ -18,17 +18,20 @@ typedef struct node{
 	struct node *next;
 }node;
 
-node *add_at_end(node *ptr,
-		 long ino,
-		 long f_size,
-		 long a_tm,
-		 long m_tm,
-		 long c_tm);
+node *tmp;
+static node *head = NULL;
+
+void printll(node *head);	//printing of linked list
 
 static int disp(const char *fpath,
-	  const struct stat *s,
-	  int typeflag,
-	  struct FTW *ftw);
+		  const struct stat *s,
+		  int typeflag,
+		  struct FTW *ftw);
+
+static int linked(const char *fpath,
+		  const struct stat *s,
+		  int typeflag,
+		  struct FTW *ftw); 
 
 int main(int argc, char *argv[]){
 	int flags = 0;
@@ -41,6 +44,8 @@ int main(int argc, char *argv[]){
 		perror("nftw");
 		return -1;
 	}
+	printf("\n");
+	nftw((argc < 2) ? "." : argv[1], linked, 20, flags);
 	exit(EXIT_SUCCESS);
 }
 
@@ -52,49 +57,63 @@ static int disp(const char *fpath,
 	printf("%-3s\t\t%7jd\t\t%-40s\t\t%ld\n",
 		(typeflag == FTW_D) ? "d" : (typeflag == FTW_F) ? "f": "???",
 		s->st_size, fpath, s->st_ino);
-	node *head = malloc(sizeof(node));
-	if(typeflag == FTW_F){
-		head->ino	=	s->st_ino;
-		head->f_size	=	s->st_size;
-		head->a_tm	=	s->st_atime;
-		head->m_tm	=	s->st_mtime;
-		head->c_tm	=	s->st_ctime;
-		head->next	= 	NULL;
-	}	
-		node *tmp = head;
-		node *ptr;
-		ptr = add_at_end(tmp, s->st_ino,
-			   s->st_size,
-			   s->st_atime,
-			   s->st_mtime,
-			   s->st_ctime);
+	return 0;
+}
+/*---------------------clear---------------------------*/
 
-		/* print func */
-		while(ptr != NULL) {
-			printf("%ld\n", ptr->ino);
-			printf("%ld\n", ptr->f_size);
-			printf("%ld\n", ptr->a_tm);
-			printf("%ld\n", ptr->m_tm);
-			printf("%ld\n", ptr->c_tm);
-			ptr = ptr->next;
-		}
+/* Linked list function */
+
+static int linked(const char *fpath,
+	  const struct stat *s,
+	  int typeflag,
+	struct FTW *ftw){ 
+	if(typeflag == FTW_F){
+		if(head == NULL){
+			head = malloc(sizeof(node));
+			head->ino	=	s->st_ino;
+			head->f_size	=	s->st_size;
+			head->a_tm	=	s->st_atime;
+			head->m_tm	=	s->st_mtime;
+			head->c_tm	=	s->st_ctime;
+			head->next	= 	NULL;
+		} tmp = head;
+		  head = tmp->next;
+		  printll(tmp);
+//		  else if(head != NULL) {
+//
+//			node *ptr = malloc(sizeof(node));
+//			ptr->ino	=	s->st_ino;
+//			ptr->f_size	=	s->st_size;
+//			ptr->a_tm	=	s->st_atime;
+//			ptr->m_tm	=	s->st_mtime;
+//			ptr->c_tm	=	s->st_ctime;
+//			ptr->next	= 	NULL;
+//
+//			
+//			printf("%ld\n", ptr->ino);
+//			printf("%ld\n", ptr->f_size);
+//			printf("%ld\n", ptr->a_tm);
+//			printf("%ld\n", ptr->m_tm);
+//			printf("%ld\n", ptr->c_tm);
+//			printf("\n");
+//			tmp->next = ptr;
+//			ptr = ptr->next;
+//		}
+	}		
 	return 0;
 }
 
-node *add_at_end(node *ptr,
-		 long ino,
-		 long f_size,
-		 long a_tm,
-		 long m_tm,
-		 long c_tm){
-		node *tmp = malloc(sizeof(node));
-		tmp->ino	=	ino;
-               	tmp->f_size	=	f_size;	
-               	tmp->a_tm   	=	a_tm;
-                tmp->m_tm	=	m_tm;
-                tmp->c_tm	=	c_tm;
-		tmp->next	=	NULL;
-
-		ptr->next = tmp;
-		return tmp;
+void printll(node *head){
+	node *ptr;
+	if(head == NULL)
+		printf("The linked list is NULL\n");
+	ptr = head;
+	while(ptr != NULL){
+		printf("%ld\n", ptr->ino);
+		printf("%ld\n", ptr->f_size);
+		printf("%ld\n", ptr->a_tm);
+		printf("%ld\n", ptr->m_tm);
+		printf("%ld\n", ptr->c_tm);
+		ptr = ptr->next;
+	}
 }
