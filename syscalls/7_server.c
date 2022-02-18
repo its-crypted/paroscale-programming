01b269eadec8fa94c4d559fa14813122e0d12858
Piyush Thange <pthange19@gmail.com>
2022-02-18 23:40:57 +0530

    Socket Programming server
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ syscalls/7_server.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netint/in.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	/* variables */
+
+
+	/* create Scoket */
+	
+	return 0;
+}
