d5f3d73833c46c098f2ddf38d0bc751aa8f8c9bb
Piyush Thange <pthange19@gmail.com>
2022-02-24 22:48:12 +0530

    asset add for totfiles & size
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- python/20_unit.py
+++ python/20_unit.py
@@ -48,7 +48,7 @@ create_random_tree(parent, file, dir, depth)
 
 while True:
     subprocess.run(["gufi_dir2index", parent, indx])
-    path = str(input("Enter the database path to check: "))
+    path = "ind/db.db"
     con = sqlite3.connect(path)
     cursor = con.cursor()
     cursor.execute("select totfiles, totsize, totltk, totmtk, totltm, totmtm, totmtg, totmtt from summary;")
@@ -61,44 +61,46 @@ while True:
     totmtm = res[0][5]
     totmtg = res[0][6]
     totmtt = res[0][7]
-#    print("The current values of DB are as follows.")
-#    print("Total files for db now", totfiles)
-#    print("Total size for db now", totsize)
-#    print("Total size of files less than or equal to 1kb(size <= 1024) now", totltk)
-#    print("Total size size greater than 1kb(size > 1024) now", totmtk)
-#    print("Total size less than or equal to 1mb(size <= 1048576) now", totltm)
-#    print("Total size greater than 1mb(size > 1048576) now", totmtm)
-#    print("Total size greater than 1gb(size > 1073741824) now", totmtg)
-#    print("Total size greater than 1tb(size > 1099511627776) now", totmtt)
+    print("The current values of DB are as follows.")
+    print("Total files: ", totfiles)
+    print("Total size: ", totsize)
+    print("Total size of files less than or equal to 1kb(size <= 1024): ", totltk)
+    print("Total size size greater than 1kb(size > 1024): ", totmtk)
+    print("Total size less than or equal to 1mb(size <= 1048576): ", totltm)
+    print("Total size greater than 1mb(size > 1048576): ", totmtm)
+    print("Total size greater than 1gb(size > 1073741824): ", totmtg)
+    print("Total size greater than 1tb(size > 1099511627776): ", totmtt)
     print()
+    dirpath = "./h1/"
+    fil = [ f for f in os.listdir( dirpath ) if os.path.isfile(dirpath + f) ] 
+    print(fil)
+    f_size = os.path.getsize(dirpath + fil)
+    print(os.listdir(dirpath))
+    if fil[0] = False:
+    	break
+    else:
+        subprocess.run(["rm", dirpath + fil[0]])
+    print("*********************< Update > ***********************>")
     subprocess.run(["gufi_dir2index", parent, indx])
     con = sqlite3.connect(path)
     cursor = con.cursor()
-    res = cursor.fetchall()
-    totfiles1 = res[0][0]
-    totsize1 = res[0][1]
-    totltk1 = res[0][2]
-    totmtk1 = res[0][3]
-    totltm1 = res[0][4]
-    totmtm1 = res[0][5]
-    totmtg1 = res[0][6]
-    totmtt1 = res[0][7]
-#    print("Total files for db now", totfiles1)
-#    print("Total size for db now", totsize1)
-#    print("Total size of files less than or equal to 1kb(size <= 1024) now", totltk1)
-#    print("Total size size greater than 1kb(size > 1024) now", totmtk1)
-#    print("Total size less than or equal to 1mb(size <= 1048576) now", totltm1)
-#    print("Total size greater than 1mb(size > 1048576) now", totmtm1)
-#    print("Total size greater than 1gb(size > 1073741824) now", totmtg1)
-#    print("Total size greater than 1tb(size > 1099511627776) now", totmtt1)
-    print("------------------------------------------------------------------")
+    cursor.execute("select totfiles, totsize, totltk, totmtk, totltm, totmtm, totmtg, totmtt from summary;")
+    re = cursor.fetchall()
+    totfiles1 = re[0][0]
+    totsize1 = re[0][1]
+    totltk1 = re[0][2]
+    totmtk1 = re[0][3]
+    totltm1 = re[0][4]
+    totmtm1 = re[0][5]
+    totmtg1 = re[0][6]
+    totmtt1 = re[0][7]
     print()
     print("----------------The updated value are as follows-------------------")
-    print("Total Files -> Old value: " + str(totfiles) + " New Value: " + str(totfiles1))
-    print("Total Size -> Old value: " + str(totsize) + " New Value: " + str(totsize1))
-    print("Total lteq kb -> Old value: " + str(totltk) + " New Value: " + str(totltk1))
-    print("Total gt kb -> Old value: " + str(totmtk) + " New Value: " + str(totmtk1))
-    print("Total lteq mb -> Old value: " + str(totltm) + " New Value: " + str(totltm1))
-    print("Total mt mb -> Old value: " + str(totmtm) + " New Value: " + str(totmtm1))
-    print("Total mt gb -> Old value: " + str(totmtg) + " New Value: " + str(totmtg1))
-    print("Total mt tb -> Old value: " + str(totmtt) + " New Value: " + str(totmtt1))
+    assert totfiles1 == totfiles - 1	     
+    assert totsize1  == totsize - f_size
+    if  assert totltk1   == totltk	       
+    assert totmtk1   == totmtk	       
+    assert totltm1   == totltm	       
+    assert totmtm1   == totmtm	       
+    assert totmtg1   == totmtg	       
+    assert totmtt1   == totmtt	       
