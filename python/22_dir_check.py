582ccb475ca1d52177299c4f9dcea49799e52a75
Piyush Thange <pthange19@gmail.com>
2022-03-08 18:49:32 +0530

    simple dir check
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

--- python/22_dir_check.py
+++ python/22_dir_check.py
@@ -11,24 +11,29 @@ for file in os.listdir(path):
        check.append(file)
 
 print(check)
-mtk = 0; mtk = 0; mtm = 0; mtg = 0
+ltk = 0; mtk = 0; mtm = 0; mtg = 0; mtt = 0
 
-for i in range(check):
+for i in range(len(check)):
 	pa = path + check[i]
 	sz = os.path.getsize(pa)
 	if sz <= 1024:
 		ltk = ltk + 1
-	elif sz > 1024 || sz < 1048575:
+	elif sz > 1024 or sz < 1048576:
 		mtk = mtk + 1
 	#	ltm = ltm + 1
-	elif sz >= 1048576 || sz <= 1073741823:
+	elif sz >= 1048576 or sz < 1073741824:
 		mtm = mtm + 1
 		#ltg 
-	elif sz >= 1073741824 || sz <= 1099511627776:
+	elif sz >= 1073741824 or sz <= 1099511627776:
 		mtg = mtg + 1
 		#ltt
 	elif sz > 1099511627776:
 		mtt = mtt + 1
 
+print(ltk)
+print(mtk)
+print(mtm)
+print(mtg)
+print(mtt)
 
 	
