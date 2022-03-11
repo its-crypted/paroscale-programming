d7815d249b881b79c1851a27dc7dff73ce1f52d6
Piyush Thange <pthange19@gmail.com>
2022-03-11 19:16:34 +0530

    Generation of tree struct
    Signed-off-by: Piyush Thange <pthange19@gmail.com>

---
+++ python/24_gen_tree.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import random
+import string
+import time
+import subprocess
+from pathlib import Path
+
+parent = sys.argv[1]
+def random_string(min_length=5, max_length=10):
+    length = random.randint(min_length, max_length)
+    for i in range(10):
+        return ''.join(
+            random.choice(string.ascii_uppercase + string.digits)
+            for _ in range(length)
+        )
+	
+def create_random_tree(parent, file=4, dir=2,
+                       depth=5):
+    os.mkdir(parent)
+    alldirs = []
+    allfiles = []
+    for i in range(int(depth)):
+        for root, dirs, files in os.walk(str(parent)):
+            for _ in range(int(dir)):
+                p = Path(root) / random_string()
+                p.mkdir(exist_ok=True)
+                alldirs.append(p)
+            for _ in range(int(file)):
+                p = Path(root) / random_string()
+                p.touch(exist_ok=True)
+                fp = open(p, 'w')
+                fp.truncate(random.randint(0,10737418242))
+                fp.close()
+                allfiles.append(p)
+            mdepth = os.path.relpath(root, str(parent)).count(os.sep)
+    alldirs = list(set(alldirs))
+    allfiles = list(set(allfiles))
+    return alldirs, allfiles
+
+#create_random_tree(parent, 3, 4, 3)
+
+if __name__ == '__main__':
+	create_random_tree(parent, 3, 4, 3)
